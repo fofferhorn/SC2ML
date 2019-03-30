94726cd0e7caef377f6a7d59a42e11e5b73fb97e
Christoffer Horn <christofferhorn@gmail.com>
2019-03-30 18:12:46 +0100

    .

--- src/test_network.py
+++ src/test_network.py
@@ -90,22 +90,35 @@ def test_network(test_data, test_labels):
     print()
 
 
-def test_network_intervals(intervals, seed, maxes_path = None, normalized_data_path = None):
-    with open('intervals.csv', 'w+') as f:
-        f.write('interval_start;interval_end;data_amount;loss;top_1_accuracy;top_3_accuracy\n')
+def test_network_intervals(intervals, seed, data_path):
+    np.random.seed(seed)
 
     model = models.load_model(FLAGS.model_name, {"top_1_categorical_accuracy": utils.top_1_categorical_accuracy, "top_3_categorical_accuracy": utils.top_3_categorical_accuracy})
-    
-    _, _, _, _, test_data, test_labels = \
-        utils.load_data_without_game_crossover(
-            FLAGS.data_path, 
-            0.7, 
-            0.15, 
-            0.15, 
-            seed,
-            maxes_path,
-            normalized_data_path
-        ) 
+
+    data_paths = []
+    for file in os.listdir(data_path):
+        file_path = os.path.join(data_path, file)
+        if os.path.isfile(file_path) and file.lower().endswith('.npy'):
+            data_paths.append(file_path)
+
+    np.random.shuffle(data_paths)
+
+    test_begin = int(len(data_paths) * (0.70 + 0.15))
+
+    test_paths = data_paths[test_begin:]
+
+    data = []
+    labels = []
+    for path in test_paths:
+        for data_point in np.load(path):
+            if len(data_point) == 248:
+                data.append(data_point[:-54])
+                labels.append(data_point[-54:])
+
+    with open('intervals.csv', 'w+') as f:
+        f.write('total_games_amount;total_data_amount\n')
+        f.write(str(len(test_paths)) + ';' + str(len(data)) + '\n\n')
+        f.write('interval_start;interval_end;data_amount;loss;top_1_accuracy;top_3_accuracy\n')
 
     prev = 0
 
@@ -113,24 +126,26 @@ def test_network_intervals(intervals, seed, maxes_path = None, normalized_data_p
 
         interval_data = []
         interval_labels = []
-        for i in range(len(test_data)):
-            if prev <= test_data[i][0] < interval_end:
-                interval_data.append(test_data[i])
-                interval_labels.append(test_labels[i])
+        for i in range(len(data)):
+            if prev <= data[i][0] < interval_end:
+                interval_data.append(data[i])
+                interval_labels.append(labels[i])
 
-        if len(interval_data) == 0:
+        if len(interval_data) == 0 or len(interval_labels) == 0:
             results = str(prev)
             results += ';' + str(interval_end)
             results += ';' + str(len(interval_data))
             results += ';;'
         else:
-            scores = model.evaluate(interval_data, interval_labels, verbose=0)
+            interval_data = keras.utils.normalize(interval_data, axis=-1, order=2)
+
+            scores = model.evaluate(np.array(interval_data), np.array(interval_labels), verbose=0)
 
             results = str(prev)
             results += ';' + str(interval_end)
             results += ';' + str(len(interval_data))
             for index in range(len(model.metrics_names)):
-                results += ';%.2f' % (scores[index]*100)
+                results += ';%.4f' % (scores[index])
 
         results += '\n'
 
@@ -170,4 +185,4 @@ if __name__ == "__main__":
 
     intervals = [672, 1344, 2016, 2688, 3360, 4032, 4704, 5376, 6048, 6720, 7392, 8064, 8736, 9408, 10080, 10752, 11424, 12096, 12768, 13440, 14112, 14784, 15456, 16128, 16800, 17472, 18144, 18816, 19488, 20160, 20832, 21504, 22176, 22848, 23520, 24192, 24864, 25536, 26208, 26880, 27552, 28224, 28896, 29568, 30240, 30912, 31584, 32256, 32928, 33600, 34272, 34944, 35616, 36288, 36960, 37632, 38304, 38976, 39648, 40320, 40992, 41664, 42336, 43008, 43680, 44352, 45024, 45696, 46368, 47040, 47712, 48384, 49056, 49728, 50400, 51072, 51744, 52416, 53088, 53760, 54432, 55104, 55776, 56448, 57120, 57792, 58464, 59136, 59808, 60480, 61152, 61824, 62496, 63168, 63840, 64512, 65184, 65856, 66528, 67200, 67872, 68544, 69216, 69888, 70560, 71232, 71904, 72576, 73248, 73920, 74592, 75264, 75936, 76608, 77280, 77952, 78624, 79296, 79968, 80640, 81312, 81984, 82656, 83328, 84000, 84672, 85344, 86016, 86688, 87360, 88032, 88704, 89376, 90048, 90720, 91392, 92064, 92736, 93408, 94080, 94752, 95424, 96096, 96768, 97440, 98112, 98784, 99456, 100128, 100800, 101472, 102144, 102816, 103488, 104160, 104832, 105504, 106176, 106848, 107520, 108192, 108864, 109536, 110208, 110880, 111552, 112224, 112896, 113568, 114240, 114912, 115584, 116256, 116928, 117600, 118272, 118944, 119616, 120288, 120960]
 
-    test_network_intervals(intervals, FLAGS.seed)
+    test_network_intervals(intervals, FLAGS.seed, FLAGS.data_path)
